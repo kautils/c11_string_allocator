5a8addb315563a14f9838f2be5432bb7bd388404
ka <todo1@rotarydef.net>
2023-10-29 00:04:34 -0700

    cmake

--- c11_string_allocator/c11_string_allocator.cmake
+++ c11_string_allocator/c11_string_allocator.cmake
@@ -1,29 +1,22 @@
-if(NOT DEFINED KAUTIL_THIRD_PARTY_DIR)
-    set(KAUTIL_THIRD_PARTY_DIR ${CMAKE_BINARY_DIR}/third_party)
-    file(MAKE_DIRECTORY "${KAUTIL_THIRD_PARTY_DIR}")
+set(${PROJECT_NAME}_m_evacu ${m})
+set(m ${PROJECT_NAME})
+list(APPEND ${m}_unsetter )
+
+if(NOT EXISTS ${CMAKE_BINARY_DIR}/CMakeKautilHeader.cmake)
+    file(DOWNLOAD https://raw.githubusercontent.com/kautils/CMakeKautilHeader/v0.0.1/CMakeKautilHeader.cmake ${CMAKE_BINARY_DIR}/CMakeKautilHeader.cmake)
 endif()
-
-macro(git_clone url)
-    get_filename_component(file_name ${url} NAME)
-    if(NOT EXISTS ${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name})
-        file(DOWNLOAD ${url} "${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name}")
-    endif()
-    include("${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name}")
-    unset(file_name)
-endmacro()
-
+include(${CMAKE_BINARY_DIR}/CMakeKautilHeader.cmake)
 git_clone(https://raw.githubusercontent.com/kautils/CMakeLibrarytemplate/v0.0.1/CMakeLibrarytemplate.cmake)
+
 
 set(module_name c11_string_allocator)
 unset(srcs)
 file(GLOB srcs ${CMAKE_CURRENT_LIST_DIR}/*.cc)
 set(${module_name}_common_pref
-    #DEBUG_VERBOSE
     MODULE_PREFIX kautil
     MODULE_NAME ${module_name}
     INCLUDES "${CMAKE_CURRENT_LIST_DIR}" $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}> $<INSTALL_INTERFACE:include> 
     SOURCES ${srcs}
-    #LINK_LIBS 
     EXPORT_NAME_PREFIX ${PROJECT_NAME}
     EXPORT_VERSION ${PROJECT_VERSION}
     EXPORT_VERSION_COMPATIBILITY AnyNewerVersion
@@ -43,3 +36,8 @@ target_link_libraries(${__t} PRIVATE ${${module_name}_static})
 target_compile_definitions(${__t} PRIVATE ${${module_name}_static_tmain_ppcs})
 
 
+foreach(__v ${${m}_unsetter})
+    unset(${__v})
+endforeach()
+unset(${m}_unsetter)
+set(m ${${PROJECT_NAME}_m_evacu})
